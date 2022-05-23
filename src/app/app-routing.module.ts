f2df78d2836baf0c68b60dcd5177819ca1ef1966
gtolbar <toledo970501@gmail.com>
2022-05-23 01:20:23 -0500

    Se agg not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListarUsuariosComponent } from './core/components/listar-usuarios/listar-usuarios.component';
+import { NotFound404Component } from './core/shared/not-found404/not-found404.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,13 @@ const routes: Routes = [
   {
     path:'listar-usuarios',
     component:ListarUsuariosComponent
+  },
+  {
+    path:'not-found-404',
+    component:NotFound404Component
+  },
+  {
+    path:'**',redirectTo:'not-found-404'
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from './core/material/material.module';
 import { ListarUsuariosComponent } from './core/components/listar-usuarios/listar-usuarios.component';
 import { RegistrarUsuarioComponent } from './core/components/registrar-usuario/registrar-usuario.component';
+import { NotFound404Component } from './core/shared/not-found404/not-found404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListarUsuariosComponent,
-    RegistrarUsuarioComponent
+    RegistrarUsuarioComponent,
+    NotFound404Component
 
   ],
   imports: [
